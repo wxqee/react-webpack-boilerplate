e6240819cfe2490add2da08906e39e481beb5ad3
Wang Xiaoqiang <xq21@vip.qq.com>
2019-01-13 19:28:41 +0800

    [antd] init

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,14 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react'],
                         plugins: ['@babel/plugin-proposal-class-properties'],
-                    },
+                        [
+                            "import",
+                            {
+                                "libraryName": "antd",
+                                "libraryDirectory": "es",
+                                "style": "css" // `style: true` 会加载 less 文件
+                            },
+                        ]                    },
                 },
             },
             {
